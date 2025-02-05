0a347233a27bf08d748935998b7717e6e3c0f689
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-05 18:41:41 +0530

    Multithreading in java

---
+++ src/multithreading/Test.java
@@ -0,0 +1,9 @@
+package multithreading;
+
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Started Multithreading in java..");
+    }
+}
