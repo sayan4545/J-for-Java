a57b7cb74434ddf25e7f10a3746b6265e3a9bf10
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-06 01:32:19 +0530

    Exploring Thread lifecycle, join()

--- src/multithreading/MyThread.java
+++ src/multithreading/MyThread.java
@@ -3,11 +3,28 @@ package multithreading;
 public class MyThread extends Thread{
     @Override
     public void run() {
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public static void main(String[] args) {
         MyThread obj = new MyThread();
         System.out.println(obj.getState());
+        obj.start();
+        System.out.println(obj.getState());
+        System.out.println(Thread.currentThread().getState());
+        try {
+            Thread.sleep(100);
+            System.out.println(obj.getState());
+            obj.join();
+            System.out.println(obj.getState());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
