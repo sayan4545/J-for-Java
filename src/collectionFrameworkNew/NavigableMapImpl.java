ff50894ed75947b7dfb9f9aa460bb99a955d8094
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-22 01:55:19 +0530

    Exploring the TreeMap class  and NavigableMap interface

---
+++ src/collectionFrameworkNew/NavigableMapImpl.java
@@ -0,0 +1,19 @@
+package collectionFrameworkNew;
+
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMapImpl {
+    public static void main(String[] args) {
+        // Navigable Map  extends SortedMap, providing more
+        // powerfull navigation ioptions such as finding the closest matching key,
+        // or retrieving the map in reverse order.
+
+        NavigableMap<Integer,String> navigableMap = new TreeMap<>();
+        navigableMap.put(1,"One");
+        navigableMap.put(2,"Two");
+        navigableMap.put(3,"Three");
+
+        System.out.println("Printing the navigable map :" + navigableMap);
+    }
+}
---
+++ src/collectionFrameworkNew/SortedMapImpl.java
@@ -0,0 +1,46 @@
+package collectionFrameworkNew;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapImpl {
+    public static void main(String[] args) {
+        // sorted map is an interface that extends the Map intefarce and ensures that
+        // the entries aere sorted eitjher according the keys or by natural oredring or by a comparator defined
+//        SortedMap<String,Integer> map1 = new TreeMap<>((a,b)->{
+//            if(b.length()-a.length()>0){
+//                return 1;
+//            }
+//            if(b.length()-a.length()<0){
+//                return -1;
+//            }
+//            else{
+//                return b.compareTo(a);
+//            }
+//        });
+//        map1.put("Ankit",100);
+//        map1.put("Chandrika",89);
+//        map1.put("Sayani",78);
+//        map1.put("Adritansha",88);
+//        System.out.println("Printing the map: "+map1);
+//
+//        //System.out.println(map1.headMap("Sayani"));
+//        //System.out.println(map1.tailMap("Chandrika"));
+//        System.out.println(map1.subMap("Ankit","Sayani"));
+        SortedMap<String, Integer> map1 = new TreeMap<>(
+                (a, b) -> {
+                    int lengthCompare = Integer.compare(b.length(), a.length());
+                    return (lengthCompare != 0) ? lengthCompare : a.compareTo(b);
+                }
+        );
+
+        map1.put("Ankit", 100);
+        map1.put("Chandrika", 89);
+        map1.put("Sayani", 78);
+        map1.put("Adritansha", 88);
+
+        System.out.println("Printing the map: " + map1);
+
+        System.out.println("SubMap Example: " + map1.subMap("Adritansha", "Sayani"));
+    }
+}
