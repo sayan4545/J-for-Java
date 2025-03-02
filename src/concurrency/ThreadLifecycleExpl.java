24455e63ee780325f0a45b1a35e83aa194c7b9b5
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-02 23:48:52 +0530

    Thread life cycle

---
+++ src/concurrency/ThreadLifecycleExpl.java
@@ -0,0 +1,50 @@
+package concurrency;
+
+public class ThreadLifecycleExpl extends Thread{
+    @Override
+    public void run(){
+        //System.out.println(Thread.currentThread().getState());
+        System.out.println("RUNNING");
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+//        for(;;){
+//            System.out.println(Thread.currentThread().getName());
+//        }
+    }
+
+    /*
+    The thread moves throgh different states throughout its execution. That is Thread lifecycle.
+    1. New --> A thread is in this state when it is created but not started.
+    2. Runnable --> After the run() method is called , the thread becomes runnable, it is ready to run
+    and is waiting CPU time.
+    3. Running --> The thread in this state is getting executed.
+    4. Blocked/Waiting --> A thread is in this state when it is weaiting for a resiource aor another thread to perform sopmem actiopn
+    5. Terminated --> A thread is in this state when it is finished executing.
+     */
+    public static void main(String[] args) {
+        ThreadLifecycleExpl t1 = new ThreadLifecycleExpl();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());// state of t1 thread
+        //System.out.println(Thread.currentThread().getState());// state of main thread
+        //System.out.println(t1.getState());
+        try{
+            Thread.sleep(1000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(t1.getState());
+        try{
+            t1.join();// join() --> The main thrtead waits for t1 to finish execution
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(t1.getState());// TERMINATED
+
+
+
+    }
+}
