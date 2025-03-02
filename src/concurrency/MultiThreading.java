a7c93a2e6b3b85bf832961a1face88515723cbd6
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-02 18:48:10 +0530

    Multithreading and Concurrency in java start

---
+++ src/concurrency/MultiThreading.java
@@ -0,0 +1,22 @@
+package concurrency;
+
+public class MultiThreading {
+    /*
+    In java multithreading is the concurrent execution of two or more threads to maximize the utilization of CPU.
+    multithreading capabilities are part of java.lang package.
+
+    In a single core environment the multithreading is handled by jvm and OS that switch between threads to
+    give an illusion of concurrency.
+    The threads share single core, time slicing is utilized to manage the thread execution.
+
+    For multi core environments, JVM distruibutes the threads to execute true parallel execution
+    of threads
+
+    When a program starts, one thread starts running immediately that is called the
+    main thread. This thread is rsponsible for execution of the main
+    method
+
+
+
+     */
+}
